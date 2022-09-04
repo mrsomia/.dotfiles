e6f0dfc531aa07766e737c7d1a2ecf2f26f1fa7c
Sachin Somia <sachinsomia@gmail.com>
2022-09-04 17:19:36 +0100

    Just adds LSP list as a comment

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -6,6 +6,8 @@ local lspconfig = require('lspconfig')
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
 local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'gopls' }
+-- list of langeuage servers available at:
+-- https://microsoft.github.io/language-server-protocol/implementors/servers/
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     -- on_attach = my_custom_on_attach,
