4a63f35ed2fb6b885c68436d4f0128d8af7239f7
Sachin Somia <sachinsomia@gmail.com>
2022-09-07 10:41:36 +0100

    adds auto formatting on save via the LSP

--- nvim/.config/nvim/lua/mrsomia/init.lua
+++ nvim/.config/nvim/lua/mrsomia/init.lua
@@ -2,3 +2,5 @@ require("mrsomia.set") -- personal sets
 require("mrsomia.packer") -- plugins
 --require("mrsomia.clip") -- Adds native clipboard intergration
 
+vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
+
