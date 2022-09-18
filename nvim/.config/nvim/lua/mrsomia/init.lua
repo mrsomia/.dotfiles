6799bea46e730e06fe565a29b6b52a35463f7a16
Sachin Somia <sachinsomia@gmail.com>
2022-09-18 22:22:00 +0100

    Adds a maps file and adds a couple of prime's maps

--- nvim/.config/nvim/lua/mrsomia/init.lua
+++ nvim/.config/nvim/lua/mrsomia/init.lua
@@ -1,6 +1,7 @@
 require("mrsomia.set") -- personal sets
 require("mrsomia.packer") -- plugins
 --require("mrsomia.clip") -- Adds native clipboard intergration
+require("mrsomia.maps")
 
 vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
 
