7f8ae6ab70f3ab48815bdd1a6f3655194ed69ec6
Sachin Somia <sachinsomia@gmail.com>
2022-12-17 13:30:35 +0000

    Fixes parsing error and deprecated errrors

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -23,21 +23,21 @@ telescope.setup {
   },
   extensions = {
     file_browser = {
-      theme = "dropdown",
-      -- disables netrw and use telescope-file-browser in its place
-      -- hijack_netrw = true,
+  --     theme = "dropdown",
+  --     -- disables netrw and use telescope-file-browser in its place
+  --     -- hijack_netrw = true,
       mappings = {
-        -- your custom insert mode mappings
-        ["i"] = {
-          ["<C-w>"] = function() vim.cmd('normal vbd') end,
-        },
+  --       -- your custom insert mode mappings
+        -- ["i"] = {
+        --   ["<C-w>"] = function() vim.cmd('normal vbd') end,
+        -- },
         ["n"] = {
           -- your custom normal mode mappings
           ["N"] = fb_actions.create,
           ["h"] = fb_actions.goto_parent_dir,
-          ["/"] = function()
-            vim.cmd('startinsert')
-          end
+          -- ["/"] = function()
+          --   vim.cmd('startinsert')
+          -- end
         },
       },
     },
