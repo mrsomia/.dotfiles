d95f4267ce658cf2dec38c3158500c238313874d
Sachin Somia <sachinsomia@gmail.com>
2023-05-16 23:00:17 +0100

    Fix: moves preview bing to half page moves

--- scripts/ff.sh
+++ scripts/ff.sh
@@ -15,7 +15,7 @@ then
     then
         fd -H --type f --exclude "node_modules" --exclude ".git/*" |
           fzf --preview "bat --color=always {1} --style=numbers" --preview-window=right,65% \
-          --bind ctrl-u:preview-page-up,ctrl-d:preview-page-down |
+          --bind ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down |
           xargs lvim -
     else
         fd --type f | fzf
@@ -26,13 +26,13 @@ else
         # if the passed arg is a dir, searches within that dir and passes this into lvim
         fd -H --type f --exclude "node_modules" --exclude ".git/*" --full-path "$PASSED" |
           fzf --preview "bat --color=always {1} --style=numbers" --preview-window=right,65% \
-          --bind ctrl-u:preview-page-up,ctrl-d:preview-page-down |
+          --bind ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down |
           xargs lvim -
     else
         # if not a dir uses the search term for the search
         fd -H --type f --exclude "node_modules" --exclude ".git/*" |
           fzf --preview "bat --color=always {1} --style=numbers" --preview-window=right,65% \
-          --bind ctrl-u:preview-page-up,ctrl-d:preview-page-down -q "$PASSED" |
+          --bind ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down -q "$PASSED" |
           xargs lvim -
     fi
 fi
