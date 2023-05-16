1cddc408db72105b358856bca32cc9836dc0ab01
Sachin Somia <sachinsomia@gmail.com>
2023-05-16 22:51:48 +0100

    Fix: Adds color column and lints with ruff

--- config/lvim/after/ftplugin/python.lua
+++ config/lvim/after/ftplugin/python.lua
@@ -1,8 +1,10 @@
 vim.cmd("setlocal tabstop=4 shiftwidth=4")
+vim.opt.colorcolumn = "88" -- Where should the highlight column be
+
 local linters = require "lvim.lsp.null-ls.linters"
 local formatters = require "lvim.lsp.null-ls.formatters"
 
-linters.setup { { command = "flake8", filetypes = { "python" } } }
+linters.setup { { command = "ruff", filetypes = { "python" } } }
 formatters.setup {
   { command = "black", filetypes = { "python" } },
 }
