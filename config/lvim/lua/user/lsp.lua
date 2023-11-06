07dae8d02bb81d9693df846c5ef2d6b9a95fcb6c
Sachin Somia <sachinsomia@gmail.com>
2023-11-06 13:00:11 +0000

    fix: Dadbod auto complete works, cmp still works

--- config/lvim/lua/user/lsp.lua
+++ config/lvim/lua/user/lsp.lua
@@ -30,6 +30,22 @@ lvim.lsp.buffer_mappings.normal_mode["[d"] = {
 -- Stop pylyzer starting up
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pylyzer" })
 
+--Remove SQLLS from skipper servers
+lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
+  return server ~= "sqlls"
+end, lvim.lsp.automatic_configuration.skipped_servers)
+
+-- Required for dabbod auto-completion <https://github.com/kristijanhusak/vim-dadbod-completion/issues/53>
+local cmp = require("cmp")
+-- To initialise dadbod auto-completion <https://github.com/kristijanhusak/vim-dadbod-completion>
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "sql", "mysql", "plsql" },
+  callback = function()
+    require('cmp').setup.buffer({ sources = { { name = 'vim-dadbod-completion' } } })
+  end,
+})
+
+-- Remove eslint from skipped servers
 lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
   return server ~= "eslint"
 end, lvim.lsp.automatic_configuration.skipped_servers)
