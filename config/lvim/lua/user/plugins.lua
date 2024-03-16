f2473ef3578ebe267ffa8fba55605911d1926c08
Sachin Somia <sachinsomia@gmail.com>
2024-03-16 14:19:17 +0000

    feat: adds noice and flash

--- config/lvim/lua/user/plugins.lua
+++ config/lvim/lua/user/plugins.lua
@@ -1,8 +1,45 @@
 lvim.plugins = {
   { "lunarvim/colorschemes" },
   {
-    'stevearc/dressing.nvim',
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      -- `nvim-notify` is only needed, if you want to use the notification view.
+      -- If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+  },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
     opts = {},
+    --    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o",               function() require("flash").remote() end,     desc = "Remote Flash" },
+      {
+        "R",
+        mode = { "o", "x" },
+        function() require("flash").treesitter_search() end,
+        desc =
+        "Treesitter Search"
+      },
+      {
+        "<c-s>",
+        mode = { "c" },
+        function() require("flash").toggle() end,
+        desc =
+        "Toggle Flash Search"
+      },
+    },
   },
   { "folke/tokyonight.nvim" },
   { "ThePrimeagen/harpoon" },
