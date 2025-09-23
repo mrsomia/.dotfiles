46e1071b2c1f2a3a54586f8956aceea11392f85e
Sachin Somia <sachinsomia@gmail.com>
2025-09-23 11:17:00 +0100

    fix(nvim): catpuccin

---
+++ config/nvim/lua/plugins/fix.lua
@@ -0,0 +1,13 @@
+return {
+  -- temp fix for https://github.com/LazyVim/LazyVim/issues/6355
+  -- PR: https://github.com/LazyVim/LazyVim/pull/6354
+  {
+    "akinsho/bufferline.nvim",
+    init = function()
+      local bufline = require("catppuccin.groups.integrations.bufferline")
+      function bufline.get()
+        return bufline.get_theme()
+      end
+    end,
+  },
+}
