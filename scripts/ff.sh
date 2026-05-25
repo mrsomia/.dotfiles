071c64238bacdc825b544eb62c246ab952d9c86c
Sachin Somia <sachinsomia@gmail.com>
2026-05-25 22:32:54 +0100

    refactor: ff script

--- scripts/ff.sh
+++ scripts/ff.sh
@@ -4,37 +4,28 @@
 # If it's a dir, search the dir
 # else search the current dir for that file
 
-IS_GIT=$(git rev-parse --is-inside-work-tree 2> /dev/null)
 PASSED=$1
+PREVIEW_CMD="bat --color=always {1} --style=numbers"
+FILE_NAME=""
 
-# Checks if not args passed
-if [ $# -eq 0 ]
-then
-    # searches and opens in nvim if this was run in a git dir
-    if [ "$IS_GIT" == 'true' ]
-    then
-        fd -H --type f --exclude "node_modules" --exclude ".git/*" |
-          fzf --preview "bat --color=always {1} --style=numbers" --preview-window=right,65% \
-          --bind ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down |
-          xargs -o nvim
-    else
-        fd -H --type f --exclude "node_modules" --exclude ".git/*" |
-          fzf --preview "bat --color=always {1} --style=numbers" --preview-window=right,65% \
-          --bind ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down |
-          xargs -o nvim
-    fi
+if [ $# -eq 0 ]; then
+  FILE_NAME=$(fd -H --type f --exclude "node_modules" --exclude ".git/*" --exclude ".jj/*" |
+    fzf --preview "$PREVIEW_CMD" --preview-window=right,65% \
+      --bind ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down)
 else
-    if [ -d "${PASSED}" ] ; then
-        # if the passed arg is a dir, searches within that dir and passes this into nvim
-        fd -H --type f --exclude "node_modules" --exclude ".git/*" --full-path "$PASSED" |
-          fzf --preview "bat --color=always {1} --style=numbers" --preview-window=right,65% \
-          --bind ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down |
-          xargs -o nvim
-    else
-        # if not a dir uses the search term for the search
-        fd -H --type f --exclude "node_modules" --exclude ".git/*" |
-          fzf --preview "bat --color=always {1} --style=numbers" --preview-window=right,65% \
-          --bind ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down -q "$PASSED" |
-          xargs -o nvim
-    fi
+  if [ -d "${PASSED}" ]; then
+    # if the passed arg is a dir, searches within that dir and passes this into nvim
+    FILE_NAME=$(fd -H --type f --exclude "node_modules" --exclude ".git/*" --exclude ".jj/*" --full-path "$PASSED" |
+      fzf --preview "$PREVIEW_CMD" --preview-window=right,65% \
+        --bind ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down)
+  else
+    # if not a dir uses the search term for the search
+    FILE_NAME=$(fd -H --type f --exclude "node_modules" --exclude ".git/*" --exclude ".jj/*" |
+      fzf --preview "$PREVIEW_CMD" --preview-window=right,65% \
+        --bind ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down -q "$PASSED")
+  fi
+fi
+
+if [[ -n $FILE_NAME ]]; then
+  nvim "$FILE_NAME"
 fi
